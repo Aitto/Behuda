00c13b3396d34b03f237af38bae3e4ac451a32a8
Aitto_Sang <32247599+Aitto@users.noreply.github.com>
2017-11-25 19:23:28 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+//#include<cmath>
+//#include<cstdlib>
+#include<algorithm>
+//#include<stack>
+//#include<cstring>
+//#include<bits/stdc++.h>
+
+#define lli long long int
+#define pf printf
+#define sf scanf
+#define pi 3.141592653589793116
+#define inf 1e9
+#define linf 1e15
+#define NULL_VALUE -999999
+#define maxNEG -99999999.0
+#define debug cout<<"Hello here!"<<endl;
+#define readFile freopen("in.txt","r",stdin);
+#define syncronize ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+int main(){
+  
+  return 0;
+}
